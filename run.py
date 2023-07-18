7c4f6f34afdaf2a48f3bc7f6bb6b46439461a6f4
Sorin Panait <126383030+SorinPan@users.noreply.github.com>
2023-07-19 01:32:36 +0200

    Create class for tic-tac-toe gameboard

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class GameBoard:
+    def __init__(self):
+        self.board = ['1', '2', '3', '4', '5', '6', '7', '8,', '9']
+
+        def print_board(self):
+            print("------------")
+            print("|", self.board[0], "|", self.board[1], "|", self.board[2], "|")
+            print("------------")
+            print("|", self.board[3], "|", self.board[4], "|", self.board[5], "|")
+            print("------------")
+            print("|", self.board[6], "|", self.board[7], "|", self.board[8], "|")
+            print("------------")
+
+
