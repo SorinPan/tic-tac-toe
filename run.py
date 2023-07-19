bbc8312c5cde2554aa69a9e2e530f491c3ebc320
Sorin Panait <126383030+SorinPan@users.noreply.github.com>
2023-07-19 02:17:56 +0200

    Create class for the gameplay logic

--- run.py
+++ run.py
@@ -13,4 +13,17 @@ class GameBoard:
             print("|", self.board[6], "|", self.board[7], "|", self.board[8], "|")
             print("------------")
 
-
+class GamePlay:
+    """
+    Represents the gamplay logic
+    """
+    
+    def __init__(self):
+        self.board = Board()
+        self.first_player = ''
+        self.second_player = 'Computer'
+        self.first_player_mark = ''
+        self.second_player_mark = ''
+        self.current_player = ''
+        self.current_mark = ''
+        
