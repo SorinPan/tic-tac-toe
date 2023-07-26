74559ec00f0d0b0b35e899a072afe995687bf08d
Sorin Panait <126383030+SorinPan@users.noreply.github.com>
2023-07-26 02:02:13 +0200

    Change dimension of the game board

--- run.py
+++ run.py
@@ -24,13 +24,19 @@ class GameBoard:
         self.board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     def print_board(self):
-        print("------------")
-        print("|", self.board[0], "|", self.board[1], "|", self.board[2], "|")
-        print("------------")
-        print("|", self.board[3], "|", self.board[4], "|", self.board[5], "|")
-        print("------------")
-        print("|", self.board[6], "|", self.board[7], "|", self.board[8], "|")
-        print("------------")
+        print("-----------------------")
+        print("|      |       |      |")
+        print(f"|  {self.board[0]}   |   {self.board[1]}   |   {self.board[2]}  |")
+        print("|      |       |      |")
+        print("-------+-------+-------")
+        print("|      |       |      |")
+        print(f"|  {self.board[3]}   |   {self.board[4]}   |   {self.board[5]}  |")
+        print("|      |       |      |")
+        print("-------+-------+-------")
+        print("|      |       |      |")
+        print(f"|  {self.board[6]}   |   {self.board[7]}   |   {self.board[8]}  |")
+        print("|      |       |      |")
+        print("-----------------------")
 
     def mark_position(self, position, mark):
         """
